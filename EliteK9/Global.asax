58d3d59b4f20f016d681ed1c5490a7000c4bd70e
caseygruse.222 <caseygruse.222@gmail.com>
2018-10-08 15:03:21 -0700

    Add project files.

---
+++ EliteK9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EliteK9.MvcApplication" Language="C#" %>
---
+++ EliteK9/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EliteK9.Startup))]
+namespace EliteK9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
