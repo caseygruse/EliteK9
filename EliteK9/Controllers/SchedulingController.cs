8111090ebc85693b3c659c78e3cab8a525c95c3c
caseygruse1 <caseygruse1@gmail.com>
2018-10-10 12:30:31 -0700

    added classes view

---
+++ EliteK9/Controllers/SchedulingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EliteK9.Controllers
+{
+    public class SchedulingController : Controller
+    {
+        // GET: Scheduling
+        public ActionResult Classes()
+        {
+            return View();
+        }
+    }
+}
