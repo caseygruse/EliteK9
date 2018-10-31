6cf15be327428954d4742315b725ca82c340f97f
caseygruse1 <caseygruse1@gmail.com>
2018-10-30 17:48:16 -0700

    working on sendgrid email

--- EliteK9/Controllers/SchedulingController.cs
+++ EliteK9/Controllers/SchedulingController.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Net.Mail;
+using System.Net.Http;
 using System.Web.Helpers;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace EliteK9.Controllers
 {
@@ -29,13 +32,54 @@ namespace EliteK9.Controllers
             if (ModelState.IsValid)
             {
 
+                SendGridMessage msg = new SendGridMessage();
 
-                MailAddress from = new MailAddress(e.SendersEmail, $"{e.FirstName}, {e.LastName}");
-                MailAddress to = new MailAddress("caseygruse.222@gmail.com");
-                MailMessage message = new MailMessage(from, to);
+                msg.SetFrom(new EmailAddress($"{e.SendersEmail}", $"{e.FirstName}, {e.LastName}"));
 
-                MailAddress bcc = new MailAddress("manager1@contoso.com");
-                
+                var recipients = new EmailAddress("caseygruse1@gmail.com", "Casey Gruse");
+
+                msg.AddTo(recipients);
+
+                msg.SetSubject("RSVP");
+
+                msg.AddContent(MimeType.Text, e.Message);
+
+                msg.MailSettings.SandboxMode = new SandboxMode();
+
+                msg.MailSettings.SandboxMode.Enable = true;
+
+                // https://docs.microsoft.com/en-us/azure/sendgrid-dotnet-how-to-send-email#how-to-create-an-email
+
+
+
+
+
+
+
+
+
+
+                //MailMessage msg = new MailMessage();
+
+                //msg.From = new MailAddress($"{e.SendersEmail}", $"{e.FirstName}, {e.LastName}");
+
+                //MailAddress recipient = new MailAddress("caseygruse1@gmail.com");
+
+                //msg.To.Add(recipient);
+
+                //msg.Subject = "RSVP";
+
+                //msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(e.Message));
+
+                //SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(465));
+
+                //System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("apikey", "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");
+
+                //smtpClient.Credentials = credentials;
+
+                //smtpClient.Send(msg);
+
+
                 return View();
             }
             return View(e);
