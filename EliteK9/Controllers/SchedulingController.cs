062186093862daab48054e1f8081125468b07614
caseygruse1 <caseygruse1@gmail.com>
2018-10-31 13:30:37 -0700

    email works

--- EliteK9/Controllers/SchedulingController.cs
+++ EliteK9/Controllers/SchedulingController.cs
@@ -27,11 +27,13 @@ namespace EliteK9.Controllers
         }
         //email send
         [HttpPost]
-        public ActionResult Schedule(EmailModel e)
+        public async System.Threading.Tasks.Task<ActionResult> Schedule(EmailModel e)
         {
             if (ModelState.IsValid)
             {
 
+                
+  /*  needs Key
                 SendGridMessage msg = new SendGridMessage();
 
                 msg.SetFrom(new EmailAddress($"{e.SendersEmail}", $"{e.FirstName}, {e.LastName}"));
@@ -42,44 +44,18 @@ namespace EliteK9.Controllers
 
                 msg.SetSubject("RSVP");
 
-                msg.AddContent(MimeType.Text, e.Message);
+                msg.PlainTextContent = e.Message;
 
-                msg.MailSettings.SandboxMode = new SandboxMode();
+                //var transportWeb = new SendGrid.Web("SENDGRID_APIKEY");
 
-                msg.MailSettings.SandboxMode.Enable = true;
+                var apiKey = System.Configuration.ConfigurationManager.AppSettings.Get("sendGridKey");
+                var client = new SendGridClient(apiKey);
+                Console.WriteLine(apiKey);
+
+                await client.SendEmailAsync(msg);
 
                 // https://docs.microsoft.com/en-us/azure/sendgrid-dotnet-how-to-send-email#how-to-create-an-email
-
-
-
-
-
-
-
-
-
-
-                //MailMessage msg = new MailMessage();
-
-                //msg.From = new MailAddress($"{e.SendersEmail}", $"{e.FirstName}, {e.LastName}");
-
-                //MailAddress recipient = new MailAddress("caseygruse1@gmail.com");
-
-                //msg.To.Add(recipient);
-
-                //msg.Subject = "RSVP";
-
-                //msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(e.Message));
-
-                //SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(465));
-
-                //System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("apikey", "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");
-
-                //smtpClient.Credentials = credentials;
-
-                //smtpClient.Send(msg);
-
-
+        */
                 return View();
             }
             return View(e);
