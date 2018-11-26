393cbdf02ddcd34ea21efe1a3686f8e7c6e5f277
caseygruse.222 <caseygruse.222@gmail.com>
2018-11-26 15:34:34 -0800

    refactered controller

--- EliteK9/Controllers/SchedulingController.cs
+++ EliteK9/Controllers/SchedulingController.cs
@@ -28,64 +28,10 @@ namespace EliteK9.Controllers
 
         public PartialViewResult CalendarEvents()
         {
-            string[] Scopes = { CalendarService.Scope.CalendarReadonly };
-            string ApplicationName = "EliteK9";
-
-
-            UserCredential credential;
-            string path = Server.MapPath("~/credentials.json");
-            using (var stream =
-                new FileStream(path, FileMode.Open, FileAccess.Read))
-            {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = Server.MapPath("~/token.json");
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                //Console.WriteLine("Credential file saved to: " + credPath);
-            }
-
-            // Create Google Calendar API service.
-            var service = new CalendarService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-            // Define parameters of request.
-            EventsResource.ListRequest request = service.Events.List("primary");
-            request.TimeMin = DateTime.Now;
-            request.ShowDeleted = false;
-            request.SingleEvents = true;
-            request.MaxResults = 10;
-            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
-
-            // List events.
-            Google.Apis.Calendar.v3.Data.Events events = request.Execute();
-            //Console.WriteLine("Upcoming events:");
-            //if (events.Items != null && events.Items.Count > 0)
-            //{
-            //    foreach (var eventItem in events.Items)
-            //    {
-            //        string when = eventItem.Start.DateTime.ToString();
-            //        if (String.IsNullOrEmpty(when))
-            //        {
-            //            when = eventItem.Start.Date;
-            //        }
-            //        Console.WriteLine("{0} ({1})", eventItem.Summary, when);
-            //    }
-            //}
-            //else
-            //{
-            //    Console.WriteLine("No upcoming events found.");
-            //}
+            Google.Apis.Calendar.v3.Data.Events events = Calendar.GetCalendarEvents();
+           
             return PartialView(events);
         }
-
 
         //get
         public ActionResult Schedule()
---
+++ EliteK9/Models/Calendar.cs
@@ -0,0 +1,62 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+
+
+namespace EliteK9.Models
+{
+    public static class Calendar
+    {
+
+        public static Google.Apis.Calendar.v3.Data.Events GetCalendarEvents()
+        {
+            string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+            string ApplicationName = "EliteK9";
+
+
+            UserCredential credential;
+            string path = HttpContext.Current.Server.MapPath("~/credentials.json");
+            using (var stream =
+                new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = HttpContext.Current.Server.MapPath("~/token.json");
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                //Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Google.Apis.Calendar.v3.Data.Events events = request.Execute();
+            return events;
+        }
+    }
+}
