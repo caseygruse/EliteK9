4e1c18ecb739497522c1410bf49716a715fecf22
caseygruse1 <caseygruse1@gmail.com>
2018-11-26 18:19:28 -0800

    created event class from google events

---
+++ EliteK9/Models/EventHelper.cs
@@ -0,0 +1,26 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EliteK9.Models
+{
+    public static class EventHelper
+    {
+        public static List<CalendarEvents> OrderEvents()
+        {
+            List<CalendarEvents> calendarEvents = new List<CalendarEvents>();
+
+            Events events = Calendar.GetCalendarEvents();
+            foreach(var i in events.Items)
+            {
+                CalendarEvents e = new CalendarEvents();
+                e.Title = i.Summary;
+                e.When = i.Start.Date;
+                calendarEvents.Add(e);
+            }
+            return calendarEvents;
+        }
+    }
+}
